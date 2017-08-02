8cdfd0a136bafe487c5d8cc70891d21d129e914e
Vincent <vincent.jumeaux@gmail.com>
2017-08-02 14:30:47 +0200

    add Gitlab Oauth Strategy & callback route
    to fix : implement callback function when provider send the user back to `auth/gitlab/callback`,

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -3,12 +3,26 @@ const __ = CONFIG.universalPath
 const _ = __.require('lib', 'utils')
 const things = require('./things/things')
 const root = require('./root/root')
+const passport = require('passport')
 
 const routes = {
   'hello': {
     get: function (req, res, next) {
       var name = req.query.name || 'you'
       res.json({ hello: name })
+    }
+  },
+  'secretPage': {
+    get: function (req, res, next) {
+      res.json({ hello: "foo" })
+    }
+  },
+  'auth/gitlab/callback': {
+    get: function (req, res, next) {
+      passport.authenticate('gitlab', {} , {
+        successRedirect: '/',
+        failureRedirect: '/hello'
+      })
     }
   }
 }
